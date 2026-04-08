0fdcd534413bafcf251c99686a83294c6dbfdc86
Walter Bender <walter@sorceo.com>
2026-04-08 16:06:39 -0400

    add license; add nl between functions

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
+// Copyright (c) 2014-26 Sugar Labs
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the The GNU Affero General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// You should have received a copy of the GNU Affero General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
 /*
   global
 
@@ -31,6 +42,7 @@ self.addEventListener("activate", function (event) {
     console.log("[PWA Builder] Claiming clients for current page");
     event.waitUntil(self.clients.claim());
 });
+
 function isPrecachedRequest(request) {
     try {
         const url = new URL(request.url);
@@ -99,6 +111,7 @@ function shouldCacheResponse(request, response) {
     // Only cache responses for allowlisted requests (static assets + explicit precache URLs).
     return isStaticAssetRequest(request) || isPrecachedRequest(request);
 }
+
 function updateCache(request, response) {
     // Cache API only supports http:// and https:// requests.
     if (!request.url.startsWith("http")) {
@@ -114,6 +127,7 @@ function updateCache(request, response) {
         return cache.put(request, response);
     });
 }
+
 function fromCache(request) {
     // Check to see if you have it in the cache
     // Return response
